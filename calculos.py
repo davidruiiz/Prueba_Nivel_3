ea5d127c84b44c14221e3223d525b935e0375615
David Ruiz <david.ruizluque@usp.ceu.es>
2023-04-26 23:47:55 +0200

    Update calculos.py

--- calculos.py
+++ calculos.py
@@ -8,3 +8,18 @@ Una vez creado el módulo, crea un script calculos.py en el mismo directorio en 
 
 """
 
+from operaciones import *
+
+ 
+
+a, b, c, d = (10, 5, 0, "Hola")
+
+ 
+
+print( "{} + {} = {}".format(a, b, suma(a, b) ) )
+
+print( "{} - {} = {}".format(b, d, resta(b, d) ) )
+
+print( "{} * {} = {}".format(b, b, producto(b, b) ) )
+
+print( "{} / {} = {}".format(a, c, division(a, c) ) )
